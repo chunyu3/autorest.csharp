8263be01279b783d0b5b7b4f9e088eceff986655
FAREAST\chunyu <chunyu@microsoft.com>
2023-02-17 16:59:41 +0800

    add collection format enum

---
+++ src/AutoRest.CSharp/Common/Input/CollectionFormat.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+
+namespace AutoRest.CSharp.Common.Input
+{
+    internal enum CollectionFormat
+    {
+        None,
+        csv,
+        ssv,
+        tsv,
+        pipes,
+        multi,
+    }
+}
